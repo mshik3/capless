64c51e1d9795ec82ff498e85feb3d6abef470596
Mustafa Shikora <mshik3@gmail.com>
2021-06-30 21:58:43 -0500

    Updates

---
+++ precache-manifest.86f0436baf2ed65db2661beff735afe0.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b071f02a91bbd605cb6e1d77bfd763e",
+    "url": "/capless/index.html"
+  },
+  {
+    "revision": "927648480eaac4669ba9",
+    "url": "/capless/static/css/main.fa22ae51.chunk.css"
+  },
+  {
+    "revision": "ef93efd0a35a3e4bf13a",
+    "url": "/capless/static/js/2.88716116.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/capless/static/js/2.88716116.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "927648480eaac4669ba9",
+    "url": "/capless/static/js/main.3580fb3c.chunk.js"
+  },
+  {
+    "revision": "0465e63cdcd90aae3b98",
+    "url": "/capless/static/js/runtime-main.cb0a51a6.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/capless/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/capless/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/capless/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/capless/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/capless/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/capless/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/capless/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/capless/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/capless/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/capless/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/capless/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/capless/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/capless/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/capless/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
