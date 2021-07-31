0d9f85075154a3b53f2ab772e1791e311961ac0b
brianrt <bthomp2000@gmail.com>
2021-07-31 01:24:23 -0500

    Begin implementation of profile cards feed

--- src/components/elements/ProfilePreview.js
+++ src/components/elements/ProfilePreview.js
@@ -13,14 +13,49 @@ const defaultProps = {
 
 const ProfilePreview = ({name}) => {
   return (
-    <button className="button-preview">
-	    <Image
+    <div className="profile-preview reveal-from-top" data-reveal-delay="200">
+      <div className = "profile-preview-header">
+      <Image
 	      src={require(`./../../assets/images/profile_images/${name}.jpeg`)}
 	      alt="Profile"
-	      width={150}
-	      height={150}
+	      width={60}
+	      height={60}
 	      className={"image-preview"}/>
-    </button>
+        <h4 className="profile-name">{name}</h4>
+      </div>
+      <p>Build the next unicorn partnership with an algorithm tailored for you</p>
+      <div className="fun-facts-container">
+        <div className="column">
+          <h5 className="facts-header">Series A</h5>
+        </div>
+        <div className="column">
+          <h5 className="facts-header">Bio Tech</h5>
+        </div>
+        <div className="column column-last">
+          <h5 className="facts-header">$20 Million</h5>
+        </div>
+      </div>
+      <div className="action-buttons">
+        <button className="action-button">
+        <Image
+          src={require(`./../../assets/images/button_images/Check.png`)}
+          alt="Yes"
+          className={"action-image"}/>
+        </button>
+        <button className="action-button">
+        <Image
+          src={require(`./../../assets/images/button_images/Chat.png`)}
+          alt="Yes"
+          className={"action-image"}/>
+        </button>
+        <button className="action-button">
+        <Image
+          src={require(`./../../assets/images/button_images/X.png`)}
+          alt="Yes"
+          className={"action-image"}/>
+        </button>
+      </div>
+    </div>
   );
 }
 
--- src/components/sections/Feed.js
+++ src/components/sections/Feed.js
@@ -3,9 +3,8 @@ import classNames from 'classnames';
 import ProfilePreview from '../elements/ProfilePreview';
 
 const Feed = () => {
- 	const outerClasses = classNames('container-xs container-feed ta-c');
+ 	const outerClasses = classNames('container-sm container-feed ta-c');
  	const startUps = ["Capless", "AskNed", "BottleUp", "Elevatr", "EverCharg", "Flash", "GreenSpork", "Iterate", "TeamSync"];
-
   return (
     <>
       <div className={outerClasses}>
@@ -14,13 +13,9 @@ const Feed = () => {
           <ProfilePreview className="container-preview-profile" name={startUps[0]}/>
           <ProfilePreview className="container-preview-profile" name={startUps[1]}/>
           <ProfilePreview className="container-preview-profile" name={startUps[2]}/>
-        </div>
-        <div>
           <ProfilePreview className="container-preview-profile" name={startUps[3]}/>
           <ProfilePreview className="container-preview-profile" name={startUps[4]}/>
           <ProfilePreview className="container-preview-profile" name={startUps[5]}/>
-        </div>
-        <div>
           <ProfilePreview className="container-preview-profile" name={startUps[6]}/>
           <ProfilePreview className="container-preview-profile" name={startUps[7]}/>
           <ProfilePreview className="container-preview-profile" name={startUps[8]}/>
