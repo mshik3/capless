437124b4237f477c2246a095eec56e69a2f7112e
Mustafa Shikora <mshik3@gmail.com>
2021-08-22 17:59:07 -0500

    making feed preview clickable

--- src/components/sections/AppContent.js
+++ src/components/sections/AppContent.js
@@ -3,11 +3,11 @@ import classNames from 'classnames';
 import Feed from './partials/Feed';
 import CompanyProfile from './partials/CompanyProfile';
 
-const AppContent = ({currentContent}) => {
+const AppContent = ({currentContent, callBack}) => {
 
   // "Daily Feed", "Company profile", "My Profile", "Settings", "Upgrade"
   const contentMap = {
-    "Daily Feed": <Feed/>,
+    "Daily Feed": <Feed callBack={callBack}/>,
     "Company profile": <CompanyProfile/>
   };
 
--- src/components/sections/partials/Feed.js
+++ src/components/sections/partials/Feed.js
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 import classNames from 'classnames';
 import ProfilePreview from '../../elements/ProfilePreview';
 
-const Feed = () => {
+const Feed = ({callBack}) => {
  	const startUps = ["Capless", "AskNed", "BottleUp", "Elevatr", "EverCharg", "Flash", "GreenSpork", "Iterate", "TeamSync"];
   const interested = [false, true, false, true, false, false, false, false, false];
   return (
     <>
       <div>
-        {startUps.map(function(startUpName, index){
-          return <ProfilePreview className="container-preview-profile" name={startUpName} isInterested={interested[index]}/>
+        {startUps.map((startUpName) => {
+          return <ProfilePreview callBack={callBack} className="container-preview-profile" name={startUpName} isInterested={interested[index]}/>
         })}
       </div>
     </>
