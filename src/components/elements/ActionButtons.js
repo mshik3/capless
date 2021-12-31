f0ec7926a212cfb8379e9a9f919eb2e3e8f0c719
Mustafa Shikora <mustafashikora@Mustafas-MacBook-Pro.local>
2021-12-31 17:22:46 -0600

    Making our match buttons less annoying

--- src/components/elements/ActionButtons.js
+++ src/components/elements/ActionButtons.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import Image from './Image';
 
@@ -11,26 +11,43 @@ const defaultProps = {
 }
 
 const ActionButtons = () => {
+    const [isShown, setIsShown] = useState(false);
+
     return (
         <div className="action-buttons">
-            <button className="action-button">
-                <Image
-                src={require(`./../../assets/images/button_images/check.svg`)}
-                alt="Yes"
-                className={"action-image"}/>
-            </button>
-            <button className="action-button">
-                <Image
-                src={require(`./../../assets/images/button_images/arrows-right-left.svg`)}
-                alt="Already Talking"
-                className={"action-image"}/>
-            </button>
-            <button className="action-button">
-                <Image
-                src={require(`./../../assets/images/button_images/cross.svg`)}
-                alt="No"
-                className={"action-image"}/>
-            </button>
+            <div className="action-button action-button-hover-parent" onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)}>
+                <button className="action-button-image">
+                    <Image
+                    src={require(`./../../assets/images/button_images/check.svg`)}
+                    alt="Yes"
+                    className={"action-image"}/>
+                </button>
+                {isShown && (<div className="action-button-hover-text">
+                    <p>Match</p>
+                </div>)}
+            </div>
+            <div className="action-button action-button-hover-parent" onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)}>
+                <button className="action-button-image">
+                    <Image
+                    src={require(`./../../assets/images/button_images/arrows-right-left.svg`)}
+                    alt="Already Talking"
+                    className={"action-image"}/>
+                </button>
+                {isShown && (<div className="action-button-hover-text">
+                    <p>Already Talking</p>
+                </div>)}
+            </div>
+            <div className="action-button action-button-hover-parent" onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)}>
+                <button className="action-button-image">
+                    <Image
+                    src={require(`./../../assets/images/button_images/cross.svg`)}
+                    alt="No"
+                    className={"action-image"}/>
+                </button>
+                {isShown && (<div className="action-button-hover-text">
+                    <p>Deny</p>
+                </div>)}
+            </div>
         </div>
     );
 }
