17d25cb264ad5ae286092ef0ffad8eee215054c1
Mustafa Shikora <mustafashikora@Mustafas-MBP.cable.rcn.com>
2022-01-02 22:13:30 -0600

    Idk, doing some shit to the settings

--- src/components/elements/SettingsRow.js
+++ src/components/elements/SettingsRow.js
@@ -4,7 +4,7 @@ import React from 'react';
 const SettingsRow = ({name}) => {
   return (
     <div className="settings-row">
-      <div id="name">{name}</div>
+      <input type="text" placeholder={name} className='settings-input'></input>
       <div id="three-dots">...</div>
     </div>
   );
