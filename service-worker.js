19b3035567aea193ceae50398bd82671bd970415
brianrt <bthomp2000@gmail.com>
2022-03-01 11:07:27 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6c4ce56bbfb14e058f07ce360b6f0686.js"
+  "/precache-manifest.dfd5d34c78c5d3a82dff06ef2cd29741.js"
 );
 
 self.addEventListener('message', (event) => {
