6407251f86d4f4b5292a9d8d6edb0b928a8951a0
brianrt <bthomp2000@gmail.com>
2022-03-19 16:41:14 -0500

    Prevent peaking at protected page for split second while pushing to history

--- src/utils/AppRoute.js
+++ src/utils/AppRoute.js
@@ -1,13 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Route, useHistory } from "react-router-dom";
 import { Auth } from 'aws-amplify'
 
-const AppRoute = ({ component: Component, layout: Layout, ...componentProps }) => {
+const AppRoute = ({ component: Component, layout: Layout, doRender=true, ...componentProps }) => {
 	return (
 		<Route
 			render={() => (
 				<Layout>
-					<Component {...componentProps} />
+					{doRender && <Component {...componentProps} />}
 				</Layout>
 			)}
 		/>
@@ -15,14 +15,18 @@ const AppRoute = ({ component: Component, layout: Layout, ...componentProps }) =
 };
 
 const ProtectedAppRoute = ({ component: Component, layout: Layout, ...componentProps }) => {
-
+	const [loggedIn, setLoggedIn] = useState(false);
 	const history = useHistory();
 	const isAuthenticated = () => {
         Auth.currentSession().then( response => {
-            if(!response.isValid()) {
+            if(response.isValid()) {
+				setLoggedIn(true);
+            } else {
+				setLoggedIn(false);
 				history.push('/signin');
-            }
+			}
         }).catch(() => {
+			setLoggedIn(false);
 			history.push('/signin');
         });
     };
@@ -31,18 +35,23 @@ const ProtectedAppRoute = ({ component: Component, layout: Layout, ...componentP
         isAuthenticated();
     }, []);
 
-	return <AppRoute component={Component} layout={Layout} {...componentProps} />
+	return <AppRoute component={Component} layout={Layout} {...componentProps} doRender={loggedIn} />
 };
 
 const UnProtectedAppRoute = ({ component: Component, layout: Layout, ...componentProps }) => {
+	const [loggedIn, setLoggedIn] = useState(true);
 	const history = useHistory();
 	const isAuthenticated = () => {
         Auth.currentSession().then( response => {
 			if (response.isValid()) {
 				// If user is signed in, go to feed
+				setLoggedIn(true);
 				history.push('/feed');
+			} else {
+				setLoggedIn(false);
 			}
         }).catch(() => {
+			setLoggedIn(false);
 			// Not signed in
         });
     };
@@ -51,7 +60,7 @@ const UnProtectedAppRoute = ({ component: Component, layout: Layout, ...componen
         isAuthenticated();
     }, []);
 
-	return <AppRoute component={Component} layout={Layout} {...componentProps} />
+	return <AppRoute component={Component} layout={Layout} {...componentProps} doRender={!loggedIn} />
 };
 
 export { AppRoute, ProtectedAppRoute, UnProtectedAppRoute };
