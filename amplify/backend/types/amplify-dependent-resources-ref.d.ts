25fbaac1b65972dad09c1c019f5b93023a6e712e
brianrt <bthomp2000@gmail.com>
2022-04-06 10:58:12 -0500

    Create new amplify project, fix for deleting cognito user pool

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
     "auth": {
-        "caplessc02fa5df": {
+        "caplessc6b59874": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
             "UserPoolId": "string",
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -2,58 +2,62 @@
 // this is an auto generated file. This will be overwritten
 
 export const getMessage = /* GraphQL */ `
-	query GetMessage($id: ID!) {
-		getMessage(id: $id) {
-			id
-			channelID
-			author
-			body
-			createdAt
-			updatedAt
-		}
-	}
+  query GetMessage($id: ID!) {
+    getMessage(id: $id) {
+      id
+      channelID
+      author
+      body
+      createdAt
+      updatedAt
+    }
+  }
 `;
 export const listMessages = /* GraphQL */ `
-	query ListMessages($filter: ModelMessageFilterInput, $limit: Int, $nextToken: String) {
-		listMessages(filter: $filter, limit: $limit, nextToken: $nextToken) {
-			items {
-				id
-				channelID
-				author
-				body
-				createdAt
-				updatedAt
-			}
-			nextToken
-		}
-	}
+  query ListMessages(
+    $filter: ModelMessageFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listMessages(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        channelID
+        author
+        body
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
 `;
 export const messagesByChannelID = /* GraphQL */ `
-	query MessagesByChannelID(
-		$channelID: ID!
-		$createdAt: ModelStringKeyConditionInput
-		$sortDirection: ModelSortDirection
-		$filter: ModelMessageFilterInput
-		$limit: Int
-		$nextToken: String
-	) {
-		messagesByChannelID(
-			channelID: $channelID
-			createdAt: $createdAt
-			sortDirection: $sortDirection
-			filter: $filter
-			limit: $limit
-			nextToken: $nextToken
-		) {
-			items {
-				id
-				channelID
-				author
-				body
-				createdAt
-				updatedAt
-			}
-			nextToken
-		}
-	}
+  query MessagesByChannelID(
+    $channelID: ID!
+    $createdAt: ModelStringKeyConditionInput
+    $sortDirection: ModelSortDirection
+    $filter: ModelMessageFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    messagesByChannelID(
+      channelID: $channelID
+      createdAt: $createdAt
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        channelID
+        author
+        body
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
 `;
